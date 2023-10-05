7eedc198ea2665562cdcfd9287578796c53b4523
Guido <guido.m.navia@gmail.com>
2023-10-05 00:33:28 -0300

    Se establece la primer view de handlebar

--- Desafio5/src/app.js
+++ Desafio5/src/app.js
@@ -6,26 +6,26 @@ import productsRouter from "./router/productsRouter.js";
 import viewsRouter from './router/viewsRouter.js';
 import { Server } from 'socket.io';
 
+console.log(__dirname + "/views");
 const app = express();
-//const express = require ('express');
 
-app.use("/api/carts", cartsRouter);
-app.use("/api/products", productsRouter);
-app.use('/api/views', viewsRouter);
+app.engine('handlebars', handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+
+app.use("/api/carts", cartsRouter);
+app.use("/api/products", productsRouter);
+app.use('/', viewsRouter);
 
 const httpServer = app.listen(8080, () => {
     console.log('Servidor levantado en el puerto 8080');
 });
 
 const socketServer = new Server(httpServer);
-
-app.engine('handlebars', handlebars.engine);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'handlebars');
 
 socketServer.on('connection', (socket) => {
     console.log('Nuevo cliente conectado!');
--- Desafio5/src/router/viewsRouter.js
+++ Desafio5/src/router/viewsRouter.js
@@ -1,9 +1,20 @@
-import express from "express";
+import { Router } from "express";
 
-const router = express.Router();
+const router = Router();
+
+const prueba = {
+    nombre: 'guido',
+    apellido: 'navia'
+}
 
 router.get('/', (req, res) => {
+    console.log("viewsRouter")
     res.render('index', {});
+});
+
+router.get('/products/view', (req, res) => {
+    console.log("viewsProducts")
+    res.render('home', { productos: prueba });
 });
 
 export default router;
